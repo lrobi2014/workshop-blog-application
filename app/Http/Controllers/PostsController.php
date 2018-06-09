926ea545cd674a4371f53de45c26bab0a411baf9
Lauren Robinson <lauren.robinson@trivago.com>
2018-06-09 23:31:41 +0300

    Add new controller, views and routes to handle the creation and display of all.

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostsController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth')->only('create');
+    }
+
+    /**
+     * Show posts.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('posts', ['posts' => Post::all()]);
+    }
+
+    /**
+     * Show a specific post
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('post')->with(
+            [
+                'post' => $post
+            ]
+        );
+    }
+
+    /**
+     * Persist post to database
+     *
+     * @param  Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+
+        $post = Auth::user()->posts()->create(
+            $request->validate([
+                'title' => 'required|string',
+                'body'  => 'required|string',
+            ])
+        );
+
+        return redirect()->route('post', $post);
+    }
+}
